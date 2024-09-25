3ea99a2adb70a7650f280b991389c38b9eca6d00
Dragan Milic <dragan@bringyour.com>
2024-09-25 14:09:51 +0200

    gvisor: rewrite source address

--- connect/ip.go
+++ connect/ip.go
@@ -231,6 +231,8 @@ func (self *LocalUserNat) Run() {
 	sourceMap := map[SourceID]EndpointAddress{}
 	sourceMapLock := sync.Mutex{}
 
+	packetTransformer := NewPacketTransformer(self.ctx)
+
 	dev, tnet, err := netstack.CreateNetTUN(nil, nil, 1500)
 	if err != nil {
 		glog.Infof("[lnr]error = %s\n", err)
@@ -250,6 +252,8 @@ func (self *LocalUserNat) Run() {
 				return
 			}
 
+			outPacket := buffer[0:n]
+
 			ipVersion := uint8(buffer[0]) >> 4
 
 			tp := TransferPath{}
@@ -260,21 +264,15 @@ func (self *LocalUserNat) Run() {
 				ipv4.DecodeFromBytes(buffer[0:n], gopacket.NilDecodeFeedback)
 				switch ipv4.Protocol {
 				case layers.IPProtocolTCP:
-					tcp := layers.TCP{}
-					tcp.DecodeFromBytes(ipv4.Payload, gopacket.NilDecodeFeedback)
 
-					sourceId := SourceID{
-						ip:   netip.AddrFrom4([4]byte(ipv4.DstIP)),
-						port: uint16(tcp.DstPort),
+					pkt, pth, err := packetTransformer.RewritePacketToVPN(buffer[0:n])
+					if err != nil {
+						glog.Infof("[lnr]rewrite error = %s\n", err)
+						continue
 					}
 
-					sourceMapLock.Lock()
-					epa := sourceMap[sourceId]
-					sourceMapLock.Unlock()
-
-					// TODO: rewrite the destination address
-
-					tp = epa.transferPath
+					tp = *pth
+					outPacket = pkt
 
 				}
 
@@ -301,7 +299,7 @@ func (self *LocalUserNat) Run() {
 
 			}
 
-			self.receive(tp, IpProtocolTcp, buffer[0:n])
+			self.receive(tp, IpProtocolTcp, outPacket)
 		}
 	}()
 
@@ -423,7 +421,13 @@ func (self *LocalUserNat) Run() {
 
 					//TODO: rewrite the source address
 
-					_, err = dev.Write(ipPacket)
+					rewritten, err := packetTransformer.RewritePacketFromVPN(ipPacket, sendPacket.source)
+					if err != nil {
+						glog.Infof("[lnr]rewrite error = %s\n", err)
+						continue
+					}
+
+					_, err = dev.Write(rewritten)
 					if err != nil {
 						glog.Infof("[lnr]write error = %s\n", err)
 					}
